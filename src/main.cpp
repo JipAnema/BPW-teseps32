3cddf572401b9675afb109529cce4ed2c9cb830c
Jip.Laptop <s1187191@student.windesheim.nl>
2024-02-01 11:33:39 +0100

    final

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,30 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
-#define DIAMETER 39
+#include <Preferences.h>
+#define DIAMETER 53
 #define MAGNEETperREV 0.5  // doe 1/aantalmagneten en donder het hier neer
 int printernummer = 1;
 
 WiFiServer server(80);
 String header;
-int count = 0;
+int count;// = 0;
 int prevcount = -1;
 unsigned long interrupt_time;
 unsigned long prev_interrupt_time;
 String servername = "http://192.168.137.1:1880/update-sensor";
 const char* getservername = "http://192.168.137.1:1880/get-sensor";
 String afstand(int revolutions);
+String httpGETRequest(const char* serverName);
+int GetMaxLengte();
 String maxlengte;
 const char* ssid     = "Test";
 const char* password = "123456789";
 unsigned int lengterol = 250000;    //Let op! mm
 float actlengte = lengterol;
 float sensorReadingsArr[9];
+bool GetNewLength = false;
+Preferences preferences;
 
 void IRAM_ATTR isr() {
   interrupt_time = millis();
@@ -30,68 +35,57 @@ void IRAM_ATTR isr() {
   }
 }
 
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-  http.begin(client, serverName);
-  int httpResponseCode = http.GET();
-  String payload = "{}"; 
-  if (httpResponseCode>0) {
-       payload = http.getString();
-  }
-  else {
-    lengterol = 250000;
-  }
-  http.end();
-  return payload;
+void IRAM_ATTR isr2() {
+  count = 0;
+  prevcount = -1;
+  GetNewLength = true;
 }
 
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  preferences.begin("counter", false);
+  count = preferences.getInt("count",0);
+  lengterol = preferences.getInt("maxlengte",250);
+  preferences.end();
   pinMode (32,INPUT_PULLUP);
   attachInterrupt(32, isr, RISING);
+  attachInterrupt(33, isr2, FALLING);
   WiFi.begin(ssid,password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-  if (WiFi.status() == WL_CONNECTED) {
-    maxlengte = httpGETRequest(getservername);
-    JSONVar myObject = JSON.parse(maxlengte);
-    if (JSON.typeof(myObject) == "undefined") {
-      lengterol = 250000;
-      actlengte = 250000;
-    }
-    else {
-     JSONVar keys = myObject.keys();
-     for (int i = 0; i < keys.length(); i++) {
-       JSONVar value = myObject[keys[i]];
-       sensorReadingsArr[i] = double(value);
-     } 
-     lengterol = sensorReadingsArr[printernummer-1];
-     actlengte = lengterol;
-   }
-  }
+  pinMode (33,INPUT_PULLUP);
   server.begin();
 }
 
 void loop() {
-     if(WiFi.status() == WL_CONNECTED) {
-       WiFiClient client;
-       HTTPClient http;
-      if (count != prevcount) {
-        http.begin(client, servername);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-        String httpSendData = "lengte="+afstand(count)+"&rolnummer=" + printernummer;
-        int httpResponseCode = http.POST(httpSendData);
-       //Voeg hier dan error handeling toe, maar fuck dat
-        http.end();
-        prevcount = count;
-       }
+  if (GetNewLength) {
+    lengterol = GetMaxLengte();
+    actlengte = lengterol;
+    GetNewLength = false;
+    preferences.begin("counter", false);
+    preferences.putInt("maxlengte",lengterol);
+    preferences.end();
+  }
+  if(WiFi.status() == WL_CONNECTED) {
+   WiFiClient client;
+   HTTPClient http;
+    if (count != prevcount) {
+      http.begin(client, servername);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      String httpSendData = "lengte="+afstand(count)+"&rolnummer=" + printernummer;
+      int httpResponseCode = http.POST(httpSendData);
+      //Voeg hier dan error handeling toe, maar fuck dat
+      http.end();
+      prevcount = count;
+      preferences.begin("counter", false);
+      preferences.putInt("count", count);
+      preferences.end();
     }
   }
+}
 
 String afstand(int revolutions) {
   if (actlengte <= (DIAMETER * 3.1415 * MAGNEETperREV) || actlengte > lengterol) {
@@ -104,4 +98,39 @@ String afstand(int revolutions) {
     sprintf(str, "%.1f", buffer);
     return (str);
   }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, serverName);
+  int httpResponseCode = http.GET();
+  String payload = "{}"; 
+  if (httpResponseCode>0) {
+    payload = http.getString();
+  }
+  else {
+    lengterol = 0;
+  }
+  http.end();
+  return payload;
+}
+
+int GetMaxLengte () {
+    if (WiFi.status() == WL_CONNECTED) {
+    maxlengte = httpGETRequest(getservername);
+    JSONVar myObject = JSON.parse(maxlengte);
+    if (JSON.typeof(myObject) == "undefined") {
+      return 0;
+    }
+    else {
+     JSONVar keys = myObject.keys();
+     for (int i = 0; i < keys.length(); i++) {
+       JSONVar value = myObject[keys[i]];
+       sensorReadingsArr[i] = double(value);
+     }
+     return sensorReadingsArr[printernummer-1];
+   }
+  }
+  return 0;
 }
